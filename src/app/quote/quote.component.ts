a58a152ae766416a1cf7e657cc8eab7697ed2a56
Ashley Mosoti <ninahb746@gmail.com>
2020-06-13 09:26:41 +0300

    Insert an add your quote button

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -18,8 +18,28 @@ export class QuoteComponent implements OnInit {
 
   quotes = [
     new Quote("Raha jipe mwenyewe", "Anonymous", "Ninah", 0, 0),
-    new Quote("What\'s coming is better than what\'s gone", "McEddie", "User1", 0, 0)
+    new Quote("Stop killing us, our lives matter", "BLM", "User1", 0, 0),
+    new Quote("Tout est beau tant que tu l'impose", "Willy William", "Mozzy", 0, 0),
+    new Quote("Inauma but itabidi uzoee", "Simple Boy", "Stivo", 0, 0)
   ];
+
+  onNew() {
+    this.quoteModel = new Quote('', '', '', 0, 0);
+    this.showNew = true;
+    this.submitType = 'save';
+  }
+  onSave() {
+    if (this.submitType === 'save') {
+      this.quotes.push(this.quoteModel);
+    }
+    this.showNew = false;
+  }
+  onCancel() {
+    this.showNew = false;
+  }
+  delete(i) {
+    this.quotes.splice(i, 1);
+  }
 
   ngOnInit(): void {
   }
