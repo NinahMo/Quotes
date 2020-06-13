c8b4b3a14e1e276d39932cc52035dd8d9e6a2d97
Ashley Mosoti <ninahb746@gmail.com>
2020-06-13 14:59:47 +0300

    Import hostlistener that defines events initiated by user actions

--- src/app/highlight.directive.ts
+++ src/app/highlight.directive.ts
@@ -1,12 +1,21 @@
-import { Directive, ElementRef } from '@angular/core';
+import { Directive, ElementRef, HostListener } from '@angular/core';
 
 @Directive({
   selector: '[appHighlight]'
 })
 export class HighlightDirective {
 
-  constructor(private elem:ElementRef) {
-    this.elem.nativeElement.style.color='maroon;'
+  constructor(private elem:ElementRef) { }
+
+  @HostListener("click") onClicks(){
+    this.textDeco("highlight-highest")
+  }
+
+  @HostListener("dbclick") onDoubleClick(){
+    this.textDeco("Marion")
+  }
+  private textDeco(action:string) {
+    this.elem.nativeElement.style.color='highlight-highest;'
    }
 
 }
